7cc94d01e7b2e138502202a457a84134359a6397
YashwanthArasJV <yashwanth.aras@saviantconsulting.com>
2024-11-27 15:22:06 +0530

    splash screen WIP

--- template/src/navigation/MainTabNavigator.tsx
+++ template/src/navigation/MainTabNavigator.tsx
@@ -2,7 +2,9 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
 import DashboardScreen from '../screens/DashboardScreen';
 import ProfileScreen from '../screens/ProfileScreen';
-import { Platform } from 'react-native';
+import { Image, Platform } from 'react-native';
+import Images from '../utils/Images';
+import Colors from '../styles/Colors';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,15 +16,39 @@ const Tab = createBottomTabNavigator();
 export default function MainTabNavigator() {
   return (
     <Tab.Navigator initialRouteName='Dashboard' screenOptions={{
-        headerShown: false,
+      headerShown: false,
+      tabBarActiveTintColor: Colors.primary,
+      tabBarInactiveTintColor: Colors.charcoal,
         tabBarStyle: {
             paddingTop: Platform.OS === 'android' ? 10 : 16,
             paddingBottom: Platform.OS === 'android' ? 10 : 24,
             height: Platform.OS === 'android' ? 70 : 80,
           },
     }}>
-      <Tab.Screen name="Dashboard" component={DashboardScreen}/>
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Home" component={DashboardScreen} options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <Image
+              source={Images.home}
+              style={{
+                height: 24,
+                width: 24,
+                tintColor: focused ? Colors.primary : Colors.charcoal,
+              }}
+            />
+          ),
+        }}/>
+      <Tab.Screen name="Profile" component={ProfileScreen} options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <Image
+              source={Images.profile}
+              style={{
+                height: 24,
+                width: 24,
+                tintColor: focused ? Colors.primary : Colors.charcoal,
+              }}
+            />
+          ),
+        }}/>
     </Tab.Navigator>
   );
 }
